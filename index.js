2c181d8c1b1968919fe9c53cf5b2e8c920a60109
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-04-02 18:55:11 -0700

    made axios use gitHubUserName to acces icon path and email path

--- index.js
+++ index.js
@@ -13,10 +13,30 @@ inquirer
   ])
   .then(answers => {
     console.log(answers.gitHubUserName);
+
+        const url = `https://api.github.com/users/${answers.gitHubUserName}`;
+
+        axios.get(url)
+        .then(function (response) {
+        // handle success
+        console.log(response.data);    
+        console.log(response.data.avatar_url);
+        console.log(response.data.email);
+        })
+
+        // is this needed???
+        // i feel like only 1 catch is needed 
+        .catch(function (error) {
+        // handle error
+        console.log(error);
+        })
+
   })
   .catch(error => {
     if(error.isTtyError) {
         console.log("There was an error");
     } 
   });
+
+
 
